e0d9b10743ef5fdaab2d33cbdcf0c362f7532cd7
andreyPedrazzani <161037920+andreyPedrazzani@users.noreply.github.com>
2024-05-17 09:48:06 -0300

    a

--- main.js
+++ main.js
@@ -5,5 +5,6 @@ let dataObjetivo = prompt("Data final ANO-MES-DIA"
 dataobjetivo = new Date (dataObjetivo + "T23:59:59");
 let diasQueFaltam = Math.floor ((dataObjetivo - 
     dataAtual) / 86400000);
+    
 document.querySelector("#dias_restantes").textContent = 
 diasQueFaltam
